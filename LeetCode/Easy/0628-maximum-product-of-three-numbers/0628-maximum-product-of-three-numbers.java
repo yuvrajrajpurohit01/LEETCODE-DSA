3b28c3f361022e644b6dbbef6f775404adae369c
Yuvraj Rajpurohit <yuvrajrajpurohitt@gmail.com>
2026-08-08 00:11:12 +0530

    Time: 16 ms (28.43%), Space: 47.6 MB (67.37%) - LeetHub

--- LeetCode/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ LeetCode/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -4,5 +4,6 @@ class Solution {
         int prod1 = nums[nums.length-2]*nums[nums.length-1]*nums[nums.length-3];
         int prod2 = nums[0]*nums[1]*nums[nums.length-1];
         return Math.max(prod1,prod2);
+
     }
 }
