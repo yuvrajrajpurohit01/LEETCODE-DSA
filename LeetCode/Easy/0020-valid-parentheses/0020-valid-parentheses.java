2c16ddc8956ab5842dc925b4bf2e643d7883db06
Yuvraj Rajpurohit <yuvrajrajpurohitt@gmail.com>
2026-08-28 17:44:25 +0530

    Time: 4 ms (36.55%), Space: 43.5 MB (15.1%) - LeetHub

---
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<Character>();
+        if(s.length()==1) return false;
+
+        for(int i = 0 ; i<s.length() ; i++){
+            if(s.charAt(i)== '(' || s.charAt(i)== '{' || s.charAt(i)== '['){
+                st.push(s.charAt(i));
+            }
+            else{
+               if(st.isEmpty()) return false;
+
+               if((st.peek() == '(' && s.charAt(i) == ')') ||
+                (st.peek() == '[' && s.charAt(i) == ']') ||
+                (st.peek() == '{' && s.charAt(i) == '}')){
+                    st.pop();
+                }
+
+                else{
+                    return false;
+                }
+
+            } 
+        }
+        return st.isEmpty();
+    }
+}
